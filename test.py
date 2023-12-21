e0886962920ed2f184fc7307be4324ef5a32c1f8
Dharshini <Dharshini@gmail.com>
2023-12-21 03:56:33 +0000

    HCF

--- test.py
+++ test.py
@@ -1,22 +1,20 @@
-# Python Program to find the L.C.M. of two input number
+# Python program to find H.C.F of two numbers
 
-def compute_lcm(x, y):
+# define a function
+def compute_hcf(x, y):
 
-   # choose the greater number
-   if x > y:
-       greater = x
-   else:
-       greater = y
+# choose the smaller number
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+    return hcf
 
-   while(True):
-       if((greater % x == 0) and (greater % y == 0)):
-           lcm = greater
-           break
-       greater += 1
-
-   return lcm
-
-num1 = 54
+num1 = 54 
 num2 = 24
 
-print("The L.C.M. is", compute_lcm(num1, num2))
+print("The H.C.F. is", compute_hcf(num1, num2))
+
